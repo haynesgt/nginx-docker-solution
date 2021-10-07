2093704064b3d2413554b8e2658532281ff7dbfc
Gavin Haynes <haynes.gt@gmail.com>
2021-10-07 20:52:47 +0000

    fix: assume logs do not change when starting

--- start.sh
+++ start.sh
@@ -9,10 +9,12 @@ source $SOURCE_DIR/env.sh
 docker stop nginx 2> /dev/null
 docker rm nginx 2> /dev/null
 
+git update-index --assume-unchanged log/ log/*
+# undo: git update-index --no-assume-unchanged log/ log/*
+
 docker run --detach --name nginx --net=host \
   --restart=always \
   -v $SOURCE_DIR/log:/var/log/nginx/ \
   -v $SOURCE_DIR/html:/usr/share/nginx/html \
   -v $SOURCE_DIR/etc:/etc/nginx \
   nginx:${NGINX_VERSION-latest}
-
